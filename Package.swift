1338d180fa29aa8ebeba4d9929651079a5b55eff
jjkaufman <jonathanjkaufman@gmail.com>
2019-10-29 20:55:27 -0700

    comment out the platform package config for now

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftProcessing",
-    platforms: [
-        .iOS(.v12),
-    ],
+//    platforms: [
+//        .iOS(.v12),
+//    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
