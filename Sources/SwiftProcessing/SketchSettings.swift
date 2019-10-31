26a46a7fc78b0131aa2d738172d3ff2630ce7866
jjkaufman <jonathanjkaufman@gmail.com>
2019-10-31 16:51:17 -0700

    update settings to support background rgb

--- Sources/SwiftProcessing/SketchSettings.swift
+++ Sources/SwiftProcessing/SketchSettings.swift
@@ -4,7 +4,14 @@
 //
 //  Created by Jonathan Kaufman on 10/31/19.
 //
+
+import UIKit
+
 public extension Sketch{
+    
+    func background(_ v1: CGFloat, _ v2: CGFloat, _ v3: CGFloat, _ a: CGFloat = 255){
+        backgroundColor = UIColor(red: v1 / 255, green: v2 / 255, blue: v3 / 255, alpha: a / 255)
+    }
     
     func fill(){
         isFill = true
