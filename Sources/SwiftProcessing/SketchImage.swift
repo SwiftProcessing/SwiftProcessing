a1a0a2bf2cb7ee590b94f7bd297483036005458d
jjkaufman <jonathanjkaufman@gmail.com>
2019-10-31 21:12:26 -0700

    init implementation of image

---
+++ Sources/SwiftProcessing/SketchImage.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jonathan Kaufman on 10/31/19.
+//
+
+import UIKit
+
+public extension Sketch{
+    func image(){
+        UIImage(named: "apple")?.draw(in: rect)
+        
+    }
+}
