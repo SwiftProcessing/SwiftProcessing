45d1754bc1adcaba11a29ed24ff013a4000ab2c7
jjkaufman <jonathanjkaufman@gmail.com>
2019-11-03 21:07:07 -0800

    make text adhere to stroke weight

--- Sources/SwiftProcessing/SketchTypography.swift
+++ Sources/SwiftProcessing/SketchTypography.swift
@@ -16,10 +16,9 @@ public extension Sketch{
     
     func text(_ str: String, _ x: CGFloat, _ y: CGFloat ){
         let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
+        
+        let attrs = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: settings.textSize)!, NSAttributedString.Key.paragraphStyle: paragraphStyle, NSAttributedString.Key.strokeWidth: NSNumber(15.0)]
 
-        let attrs = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: settings.textSize)!, NSAttributedString.Key.paragraphStyle: paragraphStyle]
-
-        str.draw(with: CGRect(x: x, y: y, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
+        str.draw(with: CGRect(x: x, y: y, width: width, height: height), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
     }
 }
