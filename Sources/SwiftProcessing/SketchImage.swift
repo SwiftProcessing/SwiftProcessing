bbf8ef53c8c3352da8d4fd9761978a24a54d480c
jjkaufman <jonathanjkaufman@gmail.com>
2019-11-05 20:36:21 -0800

    temp image logic

--- Sources/SwiftProcessing/SketchImage.swift
+++ Sources/SwiftProcessing/SketchImage.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public extension Sketch{
     
-    func image(_ name: String, _ x: CGFloat, _ y: CGFloat, _ width: CGFloat? = nil, _ height: CGFloat? = nil){
+    func createImage(_ name: String, _ width: CGFloat? = nil, _ height: CGFloat? = nil) -> CGImage{
         var w = width
         var h = height
         var image = UIImage(named: name)
@@ -19,39 +19,47 @@ public extension Sketch{
             w = image!.size.width
             h = image!.size.width
         }
+        return (image?.cgImage)!
+    }
+    
+    func image(_ name: CGImage, _ x: CGFloat, _ y: CGFloat){
+
+            let w = CGFloat(name.width)
+            let h = CGFloat(name.height)
         
-        context?.draw((image?.cgImage)!, in: CGRect(x: x, y: y, width: w!, height: h!))
-//        context?.draw(image, in: CGRec
-//        context?.draw(image, in: CGRect(
-//        context?.draw(UIImage.resize(UIImage(named: name)?, width CGSize())) .cgImage, in: CGRect)
-            
+        
+        context?.draw(name, in: CGRect(x: x, y: y, width: w, height: h))
+        //        context?.draw(image, in: CGRec
+        //        context?.draw(image, in: CGRect(
+        //        context?.draw(UIImage.resize(UIImage(named: name)?, width CGSize())) .cgImage, in: CGRect)
+        
     }
 }
 
 
 extension UIImage {
-class func resize(image: UIImage, targetSize: CGSize) -> UIImage {
-    let size = image.size
-    
-    let widthRatio  = targetSize.width  / image.size.width
-    let heightRatio = targetSize.height / image.size.height
-    
-    var newSize: CGSize
-    if widthRatio > heightRatio {
-        newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-    } else {
-        newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+    class func resize(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        var newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
     }
-    
-    let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-    
-    UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
-    image.draw(in: rect)
-    let newImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-    
-    return newImage!
-}
     
     class func scale(image: UIImage, width: CGFloat, height: CGFloat) -> UIImage? {
         let scaledSize = CGSize(width: width, height: height)
