c65c8746519b7ea28026a5378f7fdd4fc5cc6378
jjkaufman <jonathanjkaufman@gmail.com>
2019-11-27 09:01:50 -0800

    rework the image logic to support gifs, use the same function name as in P5 (loadImage), and fix bug where images are displaying upside down

--- Sources/SwiftProcessing/Core/SketchImage.swift
+++ Sources/SwiftProcessing/Core/SketchImage.swift
@@ -2,60 +2,235 @@ import UIKit
 
 public extension Sketch{
     
-    func createImage(_ name: String, _ width: CGFloat? = nil, _ height: CGFloat? = nil) -> CGImage{
-        var w = width
-        var h = height
+    func loadImage(_ name: String) -> Image{
         var image = UIImage(named: name)
-        if (w != nil && h != nil){
-            image = UIImage.scale(image: image!, width: w!, height: h!)
-        }else{
-            w = image!.size.width
-            h = image!.size.width
+        if image == nil {
+            image = UIImage.gifImageWithName(name)
         }
-        return (image?.cgImage)!
+        return Image(image!)
     }
     
-    func image(_ name: CGImage, _ x: CGFloat, _ y: CGFloat){
-
-            let w = CGFloat(name.width)
-            let h = CGFloat(name.height)
+    func image(_ image: Image, _ x: CGFloat, _ y: CGFloat, _ width: CGFloat? = nil, _ height: CGFloat? = nil){
+        let w = width == nil ? CGFloat(image.width) : width!
+        let h = height == nil ? CGFloat(image.height) : height!
         
-        
-        context?.draw(name, in: CGRect(x: x, y: y, width: w, height: h))
-        //        context?.draw(image, in: CGRec
-        //        context?.draw(image, in: CGRect(
-        //        context?.draw(UIImage.resize(UIImage(named: name)?, width CGSize())) .cgImage, in: CGRect)
-        
+        image.frame(deltaTime).draw(in: CGRect(x: x, y: y, width: w, height: h))
     }
 }
 
 
-extension UIImage {
-    class func resize(image: UIImage, targetSize: CGSize) -> UIImage {
-        let size = image.size
-        
-        let widthRatio  = targetSize.width  / image.size.width
-        let heightRatio = targetSize.height / image.size.height
-        
-        var newSize: CGSize
-        if widthRatio > heightRatio {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-        } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
-        }
-        
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
-        
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
-        image.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage!
+open class Image{
+    var uiImage: [UIImage]
+    var delay: CGFloat = 0
+    var curFrame: Int = 0
+    var deltaTime: CGFloat = 0
+    var width: CGFloat = 0
+    var height: CGFloat = 0
+    
+    init(_ image: UIImage){
+        self.width = image.size.width
+        self.height = image.size.height
+        self.uiImage = image.images != nil ? image.images! : [image]
+        self.delay = CGFloat(image.duration) / 100
     }
     
-    class func scale(image: UIImage, width: CGFloat, height: CGFloat) -> UIImage? {
-        let scaledSize = CGSize(width: width, height: height)
-        return UIImage.resize(image: image, targetSize: scaledSize)
+    func frame(_ deltaTime: CGFloat) -> UIImage{
+        if(uiImage.count == 1){
+            return uiImage[0]
+        }
+        
+        self.deltaTime = self.deltaTime + deltaTime
+        if(self.deltaTime > self.delay){
+            curFrame = curFrame + Int(self.deltaTime / self.delay)
+            self.deltaTime = 0
+        }
+        if curFrame >= uiImage.count{
+            curFrame = 0
+        }
+        return uiImage[curFrame]
+    }
+}
+
+
+
+//
+//  iOSDevCenters+GIF.swift
+//  GIF-Swift
+//  https://github.com/kiritmodi2702/GIF-Swift
+//  Created by iOSDevCenters on 11/12/15.
+//  Copyright © 2016 iOSDevCenters. All rights reserved.
+//
+import UIKit
+import ImageIO
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
+
+
+
+extension UIImage {
+    
+    public class func gifImageWithData(_ data: Data) -> UIImage? {
+        guard let source = CGImageSourceCreateWithData(data as CFData, nil) else {
+            print("image doesn't exist")
+            return nil
+        }
+        
+        return UIImage.animatedImageWithSource(source)
+    }
+    
+    public class func gifImageWithURL(_ gifUrl:String) -> UIImage? {
+        guard let bundleURL:URL? = URL(string: gifUrl)
+            else {
+                print("image named \"\(gifUrl)\" doesn't exist")
+                return nil
+        }
+        guard let imageData = try? Data(contentsOf: bundleURL!) else {
+            print("image named \"\(gifUrl)\" into NSData")
+            return nil
+        }
+        
+        return gifImageWithData(imageData)
+    }
+    
+    public class func gifImageWithName(_ name: String) -> UIImage? {
+        guard let bundleURL = Bundle.main
+            .url(forResource: name, withExtension: "gif") else {
+                print("SwiftGif: This image named \"\(name)\" does not exist")
+                return nil
+        }
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            print("SwiftGif: Cannot turn image named \"\(name)\" into NSData")
+            return nil
+        }
+        
+        return gifImageWithData(imageData)
+    }
+    
+    class func delayForImageAtIndex(_ index: Int, source: CGImageSource!) -> Double {
+        var delay = 0.1
+        
+        let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
+        let gifProperties: CFDictionary = unsafeBitCast(
+            CFDictionaryGetValue(cfProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()),
+            to: CFDictionary.self)
+        
+        var delayObject: AnyObject = unsafeBitCast(
+            CFDictionaryGetValue(gifProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()),
+            to: AnyObject.self)
+        if delayObject.doubleValue == 0 {
+            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties,
+                                                             Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()), to: AnyObject.self)
+        }
+        
+        delay = delayObject as! Double
+        
+        if delay < 0.1 {
+            delay = 0.1
+        }
+        
+        return delay
+    }
+    
+    class func gcdForPair(_ a: Int?, _ b: Int?) -> Int {
+        var a = a
+        var b = b
+        if b == nil || a == nil {
+            if b != nil {
+                return b!
+            } else if a != nil {
+                return a!
+            } else {
+                return 0
+            }
+        }
+        
+        if a < b {
+            let c = a
+            a = b
+            b = c
+        }
+        
+        var rest: Int
+        while true {
+            rest = a! % b!
+            
+            if rest == 0 {
+                return b!
+            } else {
+                a = b
+                b = rest
+            }
+        }
+    }
+    
+    class func gcdForArray(_ array: Array<Int>) -> Int {
+        if array.isEmpty {
+            return 1
+        }
+        
+        var gcd = array[0]
+        
+        for val in array {
+            gcd = UIImage.gcdForPair(val, gcd)
+        }
+        
+        return gcd
+    }
+    
+    class func animatedImageWithSource(_ source: CGImageSource) -> UIImage? {
+        let count = CGImageSourceGetCount(source)
+        var images = [CGImage]()
+        var delays = [Int]()
+        
+        for i in 0..<count {
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(image)
+            }
+            
+            let delaySeconds = UIImage.delayForImageAtIndex(Int(i),
+                                                            source: source)
+            delays.append(Int(delaySeconds * 1000.0)) // Seconds to ms
+        }
+        
+        let duration: Int = {
+            var sum = 0
+            
+            for val: Int in delays {
+                sum += val
+            }
+            
+            return sum
+        }()
+        
+        let gcd = gcdForArray(delays)
+        var frames = [UIImage]()
+        
+        var frame: UIImage
+        var frameCount: Int
+        for i in 0..<count {
+            frame = UIImage(cgImage: images[Int(i)])
+            frameCount = Int(delays[Int(i)] / gcd)
+            
+            for _ in 0..<frameCount {
+                frames.append(frame)
+            }
+        }
+        
+        let animation = UIImage.animatedImage(with: frames,
+                                              duration: Double(duration) / 1000.0)
+        
+        return animation
     }
 }
