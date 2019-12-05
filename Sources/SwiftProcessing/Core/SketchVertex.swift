f8451ab019816d7e6579cb9d16daa43c80470c57
jjkaufman <jonathanjkaufman@gmail.com>
2019-12-05 11:14:50 -0800

    adjust vertex, shape, and contour logic to support contours without erasing items already on canvas

--- Sources/SwiftProcessing/Core/SketchVertex.swift
+++ Sources/SwiftProcessing/Core/SketchVertex.swift
@@ -4,23 +4,70 @@ import UIKit
 public extension Sketch{
     
     func beginShape() {
-        context!.beginPath()
+        shapePoints = []
     }
     
     func endShape(_ mode: String = "open"){
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 2.0)
+        
+        let localContext = UIGraphicsGetCurrentContext()!
+        localContext.setFillColor(settings.fill.uiColor().cgColor)
+        localContext.setStrokeColor(settings.stroke.uiColor().cgColor)
+        
         if mode == CLOSE{
-            context!.closePath()
+            shapePoints.append(shapePoints.first!)
         }
-        context!.drawPath(using: .fillStroke)
-        context!.strokePath()
-        context!.fillPath()
+        
+        //draw shape
+        localContext.beginPath()
+        localContext.move(to: shapePoints.first!)
+        for p in shapePoints{
+            localContext.addLine(to: p)
+        }
+        localContext.drawPath(using: .fillStroke)
+        
+        //draw contour as an erase
+        localContext.beginPath()
+        localContext.setBlendMode(CGBlendMode.clear)
+        localContext.move(to: contourPoints.first!)
+        for p in contourPoints{
+            localContext.addLine(to: p)
+        }
+        localContext.drawPath(using: .fillStroke)
+        
+        //stroke contour
+        localContext.setBlendMode(CGBlendMode.normal)
+        localContext.beginPath()
+        localContext.move(to: contourPoints.first!)
+        for p in contourPoints{
+            localContext.addLine(to: p)
+        }
+        localContext.strokePath()
+        
+        //extract image from image context and display it
+        let i = Image(UIGraphicsGetImageFromCurrentImageContext()!)
+        UIGraphicsEndImageContext()
+        image(i, 0, 0)
+    }
+    
+    func beginContour(){
+        isContourStarted = true
+        contourPoints = []
+    }
+    
+    func endContour(){
+        isContourStarted = false
+        contourPoints.append(contourPoints.first!)
     }
     
     func vertex(_ x: CGFloat, _ y: CGFloat){
-        if context!.isPathEmpty{
-            context?.move(to: CGPoint(x: x, y: y))
-        }else{
-            context?.addLine(to: CGPoint(x: x, y: y))
+        let point = CGPoint(x: x, y: y)
+        
+        if isContourStarted{
+            contourPoints.append(point)
+        }
+        else{
+            shapePoints.append(point)
         }
     }
 }
