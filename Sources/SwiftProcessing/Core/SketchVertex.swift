296ba5f7786ea5586f50280f80f1335b36bb170f
jjkaufman <jonathanjkaufman@gmail.com>
2020-03-22 00:56:50 -0700

    add conditional for countour logic + alter strategy to use beginTransparencyLayer for performance reasons

--- Sources/SwiftProcessing/Core/SketchVertex.swift
+++ Sources/SwiftProcessing/Core/SketchVertex.swift
@@ -1,94 +1,88 @@
 import UIKit
 
 public extension Sketch {
-
+    
     func beginShape() {
         shapePoints = []
     }
-
+    
     func endShape(_ mode: String = "open") {
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 2.0)
-
-        let localContext = UIGraphicsGetCurrentContext()!
-        localContext.setFillColor(settings.fill.uiColor().cgColor)
-        localContext.setStrokeColor(settings.stroke.uiColor().cgColor)
+        context?.beginTransparencyLayer(auxiliaryInfo: .none)
+        
         if vertexMode == NORMAL_VERTEX {
             if mode == CLOSE {
                 shapePoints.append(shapePoints.first!)
             }
-
+            
             //draw shape
-            localContext.beginPath()
-            localContext.move(to: shapePoints.first!)
+            context?.beginPath()
+            context?.move(to: shapePoints.first!)
             for p in shapePoints {
-                localContext.addLine(to: p)
+                context?.addLine(to: p)
             }
-            localContext.drawPath(using: .fillStroke)
-
+            context?.drawPath(using: .fillStroke)
+            
             //draw contour as an erase
-            localContext.beginPath()
-            localContext.setBlendMode(CGBlendMode.clear)
-            localContext.move(to: contourPoints.first!)
-            for p in contourPoints {
-                localContext.addLine(to: p)
+            if contourPoints.count > 0 {
+                context?.beginPath()
+                context?.setBlendMode(CGBlendMode.clear)
+                context?.move(to: contourPoints.first!)
+                for p in contourPoints {
+                    context?.addLine(to: p)
+                }
+                context?.drawPath(using: .fillStroke)
+                //stroke contour
+                context?.setBlendMode(CGBlendMode.normal)
+                context?.beginPath()
+                context?.move(to: contourPoints.first!)
+                for p in contourPoints {
+                    context?.addLine(to: p)
+                }
+                context?.strokePath()
             }
-            localContext.drawPath(using: .fillStroke)
-
-            //stroke contour
-            localContext.setBlendMode(CGBlendMode.normal)
-            localContext.beginPath()
-            localContext.move(to: contourPoints.first!)
-            for p in contourPoints {
-                localContext.addLine(to: p)
-            }
-            localContext.strokePath()
         } else if vertexMode == CURVE_VERTEX {
             //todo
         } else if vertexMode == BEZIER_VERTEX {
-            localContext.move(to: shapePoints.first!)
+            context?.move(to: shapePoints.first!)
             var z = 1
             while z < shapePoints.count {
-                localContext.addCurve(to: shapePoints[z + 2], control1: shapePoints[z], control2: shapePoints[z + 1])
+                context?.addCurve(to: shapePoints[z + 2], control1: shapePoints[z], control2: shapePoints[z + 1])
                 z = z + 3
             }
-            localContext.drawPath(using: .fillStroke)
+            context?.drawPath(using: .fillStroke)
         }
-
-        //extract image from image context and display it
-        let i = Image(UIGraphicsGetImageFromCurrentImageContext()!)
-        UIGraphicsEndImageContext()
-        image(i, 0, 0)
-
+        context?.endTransparencyLayer()
+        
         shapePoints = []
         contourPoints = []
         vertexMode = NORMAL_VERTEX
     }
-
+    
     func beginContour() {
         isContourStarted = true
         contourPoints = []
     }
-
+    
     func endContour() {
         isContourStarted = false
         contourPoints.append(contourPoints.first!)
     }
-
+    
     func vertex(_ x: CGFloat, _ y: CGFloat) {
         let point = CGPoint(x: x, y: y)
-
+        
         if isContourStarted {
             contourPoints.append(point)
         } else {
             shapePoints.append(point)
         }
     }
-
+    
     func curveVertex(_ x: CGFloat, _ y: CGFloat) {
         vertexMode = CURVE_VERTEX
         vertex(x, y)
     }
-
+    
     func bezierVertex(_ x2: CGFloat, _ y2: CGFloat, _ x3: CGFloat, _ y3: CGFloat, _ x4: CGFloat, _ y4: CGFloat) {
         vertexMode = BEZIER_VERTEX
         vertex(x2, y2)
