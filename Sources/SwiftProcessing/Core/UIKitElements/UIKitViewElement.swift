3592395cc5eb1c5913356d7b65aa71e09b3ba82b
jjkaufman <jonathanjkaufman@gmail.com>
2020-04-10 15:21:44 -0700

    add functions to UIKitViewElement to help style elements

--- Sources/SwiftProcessing/Core/UIKitElements/UIKitViewElement.swift
+++ Sources/SwiftProcessing/Core/UIKitElements/UIKitViewElement.swift
@@ -14,10 +14,30 @@ open class UIKitViewElement{
         view.addSubview(element)
     }
     
+    open func borderColor(_ v1: CGFloat, _ v2: CGFloat, _ v3: CGFloat, _ a: CGFloat = 255){
+        self.element.layer.borderColor = CGColor(srgbRed: v1 / 255, green: v2 / 255, blue: v3 / 255, alpha: a / 255)
+    }
+    
+    open func borderWidth(_ w: CGFloat){
+        self.element.layer.borderWidth = w
+    }
+    
+    open func backgroundColor(_ v1: CGFloat, _ v2: CGFloat, _ v3: CGFloat, _ a: CGFloat = 255){
+        self.element.layer.backgroundColor = CGColor(srgbRed: v1 / 255, green: v2 / 255, blue: v3 / 255, alpha: a / 255)
+    }
+   
+    open func opacity(_ o: CGFloat){
+        self.element.layer.opacity = Float(o)
+    }
+    
+    open func cornerRadius(_ r: CGFloat){
+        self.element.layer.cornerRadius = r
+    }
+    
     open func size(_ w: CGFloat, _ h: CGFloat){
         element.frame = CGRect(x: element.frame.minX, y: element.frame.minY, width: w, height: h)
     }
-       
+    
     open func position(_ x: CGFloat, _ y: CGFloat){
         element.frame = CGRect(x: x, y: y, width: element.frame.width, height: element.frame.height)
     }
