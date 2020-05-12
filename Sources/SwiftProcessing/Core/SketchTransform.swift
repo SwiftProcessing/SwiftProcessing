819e19180ec1e3d240f859929f6d3b1c55419eaa
jjkaufman <jonathanjkaufman@gmail.com>
2020-05-11 20:51:48 -0700

    add ability to retrieve the current translation and scale of the sketch

--- Sources/SwiftProcessing/Core/SketchTransform.swift
+++ Sources/SwiftProcessing/Core/SketchTransform.swift
@@ -1,15 +1,31 @@
 import UIKit
 
 public extension Sketch {
-
+    
+    var translation: Vector{
+        get{
+            let translationX = context == nil ? 0 : (context?.ctm.tx)! / UIScreen.main.scale
+            let translationY = context == nil ? 0 :  -(context?.ctm.ty)! / UIScreen.main.scale + frame.height
+            return createVector(translationX, translationY)
+        }
+    }
+    
+    var scale: Vector{
+        get{
+            let scaleX = context == nil ? 1 : (context?.ctm.a)! / UIScreen.main.scale
+            let scaleY = context == nil ? 1 :  -(context?.ctm.d)! / UIScreen.main.scale
+            return createVector(scaleX, scaleY)
+        }
+    }
+    
     func rotate(_ angle: CGFloat) {
         context?.rotate(by: angle)
     }
-
+    
     func translate(_ x: CGFloat, _ y: CGFloat) {
         context?.translateBy(x: x, y: y)
     }
-
+    
     func scale(_ x: CGFloat, _ y: CGFloat) {
         context?.scaleBy(x: x, y: y)
     }
