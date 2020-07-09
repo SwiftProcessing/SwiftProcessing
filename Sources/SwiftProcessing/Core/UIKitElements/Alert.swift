2fe98456b4858072f6d7835319d633e201528614
jjkaufman <jonathanjkaufman@gmail.com>
2020-07-09 12:57:42 -0700

    adjust alert to follow create* pattern and be easier to use

--- Sources/SwiftProcessing/Core/UIKitElements/Alert.swift
+++ Sources/SwiftProcessing/Core/UIKitElements/Alert.swift
@@ -11,18 +11,30 @@ open class Alert {
     
     init(_ error: NSError) {
         self.alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-        self.addAction(title: "Ok", style: .cancel, handler: nil )
+        self.addAction("Ok")
     }
     
-    public  func show() {
+    public func show() {
         DispatchQueue.main.async {
             UIApplication.topViewController?.present(self.alert, animated: true, completion: nil)
         }
     }
     
-    public func addAction(title: String, style: UIAlertAction.Style = UIAlertAction.Style.default, handler: ((UIAlertAction) -> Void)?) {
+    public func addAction(_ title: String, _ style: UIAlertAction.Style = UIAlertAction.Style.default, handler: ((UIAlertAction) -> Void)?) {
         self.alert.addAction(UIAlertAction(title: title, style: style, handler: handler))
+    }
+    
+    public func addAction(_ title: String, _ style: UIAlertAction.Style = UIAlertAction.Style.default, handler: (() -> Void)? = nil) {
+        self.alert.addAction(UIAlertAction(title: title, style: style, handler: {
+            action in
+            handler?()
+        }))
     }
     
 }
 
+public extension Sketch {
+    func createAlert(_ title: String, _ message: String) -> Alert{
+        return Alert(title, message)
+    }
+}
