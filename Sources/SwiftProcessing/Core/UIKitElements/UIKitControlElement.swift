d650f6663a9e094db625248d8962d3c55563163f
jjkaufman <jonathanjkaufman@gmail.com>
2020-07-23 15:31:39 -0700

    add additional tap gesture reconizer to UIControl Event to help catalyst apps

--- Sources/SwiftProcessing/Core/UIKitElements/UIKitControlElement.swift
+++ Sources/SwiftProcessing/Core/UIKitElements/UIKitControlElement.swift
@@ -1,17 +1,32 @@
 import Foundation
 import UIKit
 
-open class UIKitControlElement : UIKitViewElement{
+open class UIKitControlElement : UIKitViewElement, UIGestureRecognizerDelegate{
     open var touchUpAction: () -> Void = {}
     open var touchDownAction: () -> Void = {}
     open var valueChangedAction: () -> Void = {}
 
     override init(_ view: Sketch, _ element: UIView) {
         super.init(view, element)
+        
         (element as! UIControl).addTarget(self, action: #selector(touchUpInsideHelper(_:)), for: .touchUpInside)
         (element as! UIControl).addTarget(self, action: #selector(touchDownHelper(_:)), for: .touchDown)
         (element as! UIControl).addTarget(self, action: #selector(valueChangedHelper(_:)), for: .valueChanged)
+        
+        let singleTapGesture = UITapGestureRecognizer(target: self, action: #selector(touchUpInsideHelper))
+        singleTapGesture.numberOfTapsRequired = 1
+        singleTapGesture.delegate = self
+        singleTapGesture.cancelsTouchesInView = false
+        element.addGestureRecognizer(singleTapGesture)
+            
     }
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+          if (touch.view as? UIControl) != nil{
+              return true
+          }else{
+              return false
+          }
+      }
     
     @objc func touchUpInsideHelper(_ sender: UIView) {
        touchUpAction()
