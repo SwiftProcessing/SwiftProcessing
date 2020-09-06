7f128bfe7542c64d2f4d0ee1293f6f69ec4f1af7
jjkaufman <jonathanjkaufman@gmail.com>
2020-09-06 15:22:42 -0700

    Update ImagePicker.swift

--- Sources/SwiftProcessing/Core/Images/ImagePicker.swift
+++ Sources/SwiftProcessing/Core/Images/ImagePicker.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 open class ImagePicker: NSObject {
-
+    
     private let pickerController: UIImagePickerController
     private weak var presentationController: UIViewController?
     
@@ -19,58 +19,28 @@ open class ImagePicker: NSObject {
     public init(_ sketch: Sketch, _ presentationController: UIViewController) {
         self.pickerController = UIImagePickerController()
         self.sketch = sketch
-
+        
         super.init()
-
+        
         self.presentationController = presentationController
-
+        
         self.pickerController.delegate = self
         self.pickerController.allowsEditing = true
         self.pickerController.mediaTypes = ["public.image"]
     }
-
-    private func action(for type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
+    
+    public func show(for type: UIImagePickerController.SourceType, _ picked: @escaping (Image) -> Void) {
+        
         guard UIImagePickerController.isSourceTypeAvailable(type) else {
-            return nil
-        }
-
-        return UIAlertAction(title: title, style: .default) { [unowned self] _ in
-            self.pickerController.sourceType = type
-            self.presentationController?.present(self.pickerController, animated: true)
-        }
-    }
-
-    public func show(_ from: UIView? = nil, _ picked: @escaping (Image) -> Void) {
-        var sourceView: UIView! = from
-        if from == nil {
-            sourceView = sketch
+            return
         }
         
         pickedAction = picked
         
-        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-
-        if let action = self.action(for: .camera, title: "Take photo") {
-            alertController.addAction(action)
-        }
-        if let action = self.action(for: .savedPhotosAlbum, title: "Camera roll") {
-            alertController.addAction(action)
-        }
-        if let action = self.action(for: .photoLibrary, title: "Photo library") {
-            alertController.addAction(action)
-        }
-
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            alertController.popoverPresentationController?.sourceView = sourceView
-            alertController.popoverPresentationController?.sourceRect = sourceView!.bounds
-            alertController.popoverPresentationController?.permittedArrowDirections = [.down, .up]
-        }
-
-        self.presentationController?.present(alertController, animated: true)
+        self.pickerController.sourceType = type
+        self.presentationController?.present(self.pickerController, animated: true)
     }
-
+    
     private func pickerController(_ controller: UIImagePickerController, didSelect image: UIImage?) {
         controller.dismiss(animated: true, completion: nil)
         if let i = image {
@@ -85,11 +55,11 @@ open class ImagePicker: NSObject {
 }
 
 extension ImagePicker: UIImagePickerControllerDelegate {
-
+    
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.pickerController(picker, didSelect: nil)
     }
-
+    
     public func imagePickerController(_ picker: UIImagePickerController,
                                       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         guard let image = info[.editedImage] as? UIImage else {
