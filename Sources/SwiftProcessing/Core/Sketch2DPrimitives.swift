7e9c8f6d4ab72e79385613ac50f129207378eddd
jjkaufman <jonathanjkaufman@gmail.com>
2020-10-15 15:43:51 -0700

    init version of documentation

--- Sources/SwiftProcessing/Core/Sketch2DPrimitives.swift
+++ Sources/SwiftProcessing/Core/Sketch2DPrimitives.swift
@@ -1,8 +1,76 @@
 import UIKit
 
-public extension Sketch {
+/// Draw 2D Primitives to the screen
+public protocol Shapes {
+    
+    /// Draw an arc to the screen.
+    /// - Parameters:
+    ///   - x: x-coordinate of the arc's ellipse
+    ///   - y:  y-coordinate of the arc's ellipse
+    ///   - w: width of the arc's ellipse by default
+    ///   - h: height of the arc's ellipse by default
+    ///   - start: angle to start the arc, specified in radians
+    ///   - stop: angle to stop the arc, specified in radians
+    ///   - mode:  optional parameter to determine the way of drawing the arc. either CHORD, PIE or OPEN
+    func arc(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat, _ start: CGFloat, _ stop: CGFloat, _ mode: String)
+    
+    /// Draw an ellipse to the screen.
+    /// - Parameters:
+    ///   - x: x-coordinate of the center of ellipse.
+    ///   - y: y-coordinate of the center of ellipse.
+    ///   - w: width of the ellipse.
+    ///   - h: height of the ellipse. (optional)
+    func ellipse(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat)
+    
+    /// Draw a circle to the screen
+    /// - Parameters:
+    ///   - x: x-coordinate of the centre of the circle.
+    ///   - y: y-coordinate of the centre of the circle.
+    ///   - d: diameter of the circle.
+    func circle(_ x: CGFloat, _ y: CGFloat, _ d: CGFloat)
+    
+    /// Draw a line to the screen.
+    /// - Parameters:
+    ///   - x1: the x-coordinate of the first point
+    ///   - y1: the y-coordinate of the first point
+    ///   - x2: the x-coordinate of the second point
+    ///   - y2: the y-coordinate of the second point
+    func line(_ x1: CGFloat, _ y1: CGFloat, _ x2: CGFloat, _ y2: CGFloat)
+    
+    /// Draws a single point on the screen using the current stroke weight.
+    /// - Parameters:
+    ///   - x: the x-coordinate
+    ///   - y: the y-coordinate
+    func point(_ x: CGFloat, _ y: CGFloat)
+    
+    /// Draws a rectangle on the screen
+    /// - Parameters:
+    ///   - x: x-coordinate of the rectangle.
+    ///   - y: y-coordinate of the rectangle.
+    ///   - w: width of the rectangle.
+    ///   - h: height of the rectangle. (Optional)
+    func rect(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat)
+    
+    /// Draws a square to the screen.
+    /// - Parameters:
+    ///   - x: x-coordinate of the square.
+    ///   - y: y-coordinate of the square.
+    ///   - s: size of the square.
+    func square(_ x: CGFloat, _ y: CGFloat, _ s: CGFloat)
+    
+    /// Draws a trangle to the screen.
+    /// - Parameters:
+    ///   - x1: x-coordinate of the first point
+    ///   - y1: y-coordinate of the first point
+    ///   - x2: x-coordinate of the second point
+    ///   - y2: y-coordinate of the second point
+    ///   - x3: x-coordinate of the third point
+    ///   - y3: y-coordinate of the third point
+    func triangle(_ x1: CGFloat, _ y1: CGFloat, _ x2: CGFloat, _ y2: CGFloat, _ x3: CGFloat, _ y3: CGFloat)
+}
 
-    func arc(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat, _ start: CGFloat, _ stop: CGFloat, _ mode: String = "pie") {
+extension Sketch: Shapes {
+    public func arc(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat, _ start: CGFloat, _ stop: CGFloat, _ mode: String = "pie") {
  
         let r = w * 0.5
         let t = CGAffineTransform(scaleX: 1.0, y: h / w)
@@ -25,7 +93,7 @@ public extension Sketch {
         context?.drawPath(using: .eoFillStroke)
     }
 
-    func ellipse(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat = -1 ) {
+    public func ellipse(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat = -1 ) {
         var height = h
         if h == -1 {
             height = w
@@ -46,7 +114,7 @@ public extension Sketch {
         pop()
     }
 
-    func circle(_ x: CGFloat, _ y: CGFloat, _ d: CGFloat) {
+    public func circle(_ x: CGFloat, _ y: CGFloat, _ d: CGFloat) {
         ellipse(x, y, d, d)
     }
 
@@ -65,26 +133,26 @@ public extension Sketch {
         }
     }
 
-    func line(_ x1: CGFloat, _ y1: CGFloat, _ x2: CGFloat, _ y2: CGFloat) {
+    public func line(_ x1: CGFloat, _ y1: CGFloat, _ x2: CGFloat, _ y2: CGFloat) {
         context?.move(to: CGPoint(x: x1, y: y1))
         context?.addLine(to: CGPoint(x: x2, y: y2))
         context?.strokePath()
     }
 
-    func point(_ x: CGFloat, _ y: CGFloat) {
+    public func point(_ x: CGFloat, _ y: CGFloat) {
         line(x, y, x + strokeWeight, y)
     }
 
-    func rect(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat) {
+    public func rect(_ x: CGFloat, _ y: CGFloat, _ w: CGFloat, _ h: CGFloat) {
         context?.stroke(CGRect(x: x, y: y, width: w, height: h))
         context?.fill(CGRect(x: x, y: y, width: w, height: h))
     }
 
-    func square(_ x: CGFloat, _ y: CGFloat, _ s: CGFloat) {
+    public func square(_ x: CGFloat, _ y: CGFloat, _ s: CGFloat) {
         rect(x, y, s, s)
     }
 
-    func triangle(_ x1: CGFloat, _ y1: CGFloat, _ x2: CGFloat, _ y2: CGFloat, _ x3: CGFloat, _ y3: CGFloat) {
+    public func triangle(_ x1: CGFloat, _ y1: CGFloat, _ x2: CGFloat, _ y2: CGFloat, _ x3: CGFloat, _ y3: CGFloat) {
         context?.beginPath()
         context?.move(to: CGPoint(x: x1, y: y1))
         context?.addLine(to: CGPoint(x: x2, y: y2))
