e3ec14f1c2f8998d967254d8e095f825f0fc916c
jjkaufman <jonathanjkaufman@gmail.com>
2020-10-16 12:45:30 -0700

    Attributes

--- Sources/SwiftProcessing/Core/SketchAttributes.swift
+++ Sources/SwiftProcessing/Core/SketchAttributes.swift
@@ -1,21 +1,39 @@
 import Foundation
 import UIKit
 
-public extension Sketch {
-    func strokeWeight(_ weight: CGFloat) {
+/// Atributes used for drawing
+public protocol Attributes {
+    
+    /// Sets the width of the stroke used for lines, points and the border around shapes.
+    /// - Parameter weight: the weight of the stroke
+    func strokeWeight(_ weight: CGFloat)
+    
+    /// Draws all geometry with smooth (anti-aliased) edges
+    func smooth()
+    
+    /// Draws all geometry with jagged (aliased) edges
+    func noSmooth()
+    
+    /// Modifies the location from which ellipses, circles, and arcs are drawn. The default mode is CENTER.
+    /// - Parameter eMode: either CENTER, RADIUS, CORNER, or CORNERS
+    func ellipseMode(_ eMode: String)
+}
+
+extension Sketch: Attributes {
+    public func strokeWeight(_ weight: CGFloat) {
         context?.setLineWidth(weight)
         settings.strokeWeight = weight
     }
 
-    func smooth() {
+    public func smooth() {
         context?.setShouldAntialias(true)
     }
 
-    func noSmooth() {
+    public func noSmooth() {
         context?.setShouldAntialias(false)
     }
 
-    func ellipseMode(_ eMode: String) {
+    public func ellipseMode(_ eMode: String) {
         settings.ellipseMode = eMode
     }
 }
