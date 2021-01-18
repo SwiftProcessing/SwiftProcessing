80ea55938904c867c73d4b40bb70484e1d92d450
jjkaufman <jonathanjkaufman@gmail.com>
2021-01-18 10:17:16 -0800

    adjust loop function to also add the fps timer to the tracking mode

--- Sources/SwiftProcessing/Core/SketchStructure.swift
+++ Sources/SwiftProcessing/Core/SketchStructure.swift
@@ -1,28 +1,28 @@
 import UIKit
 
 public extension Sketch {
-
+    
     func loop() {
         if #available(iOS 10.0, *) {
-           
+            
             fpsTimer = CADisplayLink(target: self,
-                                            selector: #selector(nextFrame))
+                                     selector: #selector(nextFrame))
             fpsTimer?.preferredFramesPerSecond = Int(fps)
-            fpsTimer!.add(to: .main,
-                            forMode: RunLoop.Mode.default)
-            fpsTimer!.add(to: .main,
-                            forMode: RunLoop.Mode.tracking)
+            fpsTimer!.add(to: .main, forMode: RunLoop.Mode.default)
+            fpsTimer!.add(to: .main, forMode: RunLoop.Mode.tracking)
         } else {
             // Fallback on earlier versions
         }
     }
+    
     @objc func nextFrame(displaylink: CADisplayLink) {
         self.setNeedsDisplay()
     }
+    
     func noLoop() {
         fpsTimer!.invalidate()
     }
-
+    
     func redraw() {
         self.setNeedsDisplay()
     }
